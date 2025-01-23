cae3d4d32e8e5214e5043721ea381d884c6eaa28
Zhanuzak Sayassat <sayassat.zhanuzak@gmail.com>
2025-01-23 16:55:17 +0500

    These are data types

---
+++ Lab_1/4.py
@@ -0,0 +1,11 @@
+x = str(3) # x is string, so it will display '3'
+y = int(3) # y is integer, so it will display 3
+z = float(3) # z is floating point number, so it will display 3.0
+print(x)
+print(y)
+print(z) 
+
+t = "Sayassat"
+u = 4
+print(type(t))
+print(type(u))
