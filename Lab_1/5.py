cfe81a3bbe70d0aae35382e3cd6dd9b2da4f800b
Zhanuzak Sayassat <sayassat.zhanuzak@gmail.com>
2025-01-23 17:26:49 +0500

    Numbers

--- Lab_1/5.py
+++
@@ -1,8 +0,0 @@
-x = 5
-y = "sweet" #string
-z = 't'     #also string
-u = 5.0
-print(type(x))
-print(type(y))
-print(type(z))
-print(type(u))
---
+++ Lab_1/variables.py
@@ -0,0 +1,15 @@
+x = 5
+y = "sweet" #string
+z = 't'     #also string
+u = 5.0
+t = 5j      #complex
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(u))
+print(type(t))
+
+x = float(1)     # x will be 1.0
+y = float(2.8)   # y will be 2.8
+z = float("3")   # z will be 3.0
+w = float("4.2") # w will be 4.2
