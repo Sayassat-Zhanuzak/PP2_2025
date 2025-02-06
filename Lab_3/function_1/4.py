f1920b813533c993e427a9f863a876740a5e2223
Zhanuzak Sayassat <sayassat.zhanuzak@gmail.com>
2025-02-06 22:21:19 +0500

    Solved Problem 4

--- Lab_3/function_1/4.py
+++ Lab_3/function_1/4.py
@@ -10,6 +10,9 @@ def filter_prime(lst):
 
     primes = [nums for nums in lst if is_prime(nums)]
     return primes
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+result_1 = filter_prime(nums)
+print(result_1)
 
 user_input = input("Enter elements: ")
 numbers = list(map(int, user_input.split())) #list converts iterator that is from map to list
