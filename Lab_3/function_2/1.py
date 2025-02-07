353594f9dfc24191959e39f338c2410ddbd1acd3
Zhanuzak Sayassat <sayassat.zhanuzak@gmail.com>
2025-02-07 10:31:56 +0500

    Solved Problem 14

--- Lab_3/function_2/1.py
+++ Lab_3/function_2/1.py
@@ -1,18 +1,15 @@
 from movies_data import movies
 
 def is_highly_rated(movie):
-    """Returns True if the movie's IMDB rating is above 5.5"""
     return movie["imdb"] > 5.5
 
 def find_movie(movie_name):
-    """Searches for a movie by name in the movies list"""
     for movie in movies:
         if movie["name"].lower() == movie_name.lower():
             return movie
     return None
 
 def check_movie():
-    """Gets user input and checks if the movie has an IMDB score above 5.5"""
     movie_name = input("Enter the movie name: ").strip()
     movie = find_movie(movie_name)
 
